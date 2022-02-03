474d414acc3724de78eec166cc3b4af9c10cf4a7
ahtaxam03@gmail.com <ahtaxam03@gmail.com>
2022-02-03 11:38:16 +0500

    Added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Login from "./components/Login";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./components/Home";
+import Rooms from './components/Rooms'
+import AddRoom from "./components/AddRoom";
+import UpdateRoom from "./components/UpdateRoom";
+import RoomsRecord from "./components/RoomsRecord";
+import RegisterStudent from "./components/students/RegisterStudent";
+import UpdateStudent from "./components/students/UpdateStudent";
+import LivingStudents from "./components/students/LivingStudents";
+import SudentsFee from "./components/students/SudentsFee";
+import HostelExpense from "./components/HostelExpense"
+import NewEmployee from "./components/Employees/NewEmployee";
+import UpdateEmployee from "./components/Employees/UpdateEmployee";
+import EmployeePayment from "./components/Employees/EmployeePayment";
+import EmployeeRecord from "./components/Employees/EmployeeRecord";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element = {<Login/>}  ></Route>
+          <Route path = '/home' element = {<Home/>} />
+          <Route path = '/rooms' element = {<Rooms/>}></Route>
+          <Route path = "/rooms/addroom" element = {<AddRoom/>}/>
+          <Route path = "/rooms/updateroom" element = {<UpdateRoom/>}/>
+          <Route path='/rooms/record' element = {<RoomsRecord/>}></Route>
+
+          <Route path='/registerstudent'  element={<RegisterStudent/>}/>
+          <Route path='/updatestudent' element={<UpdateStudent/>}/>
+          <Route path='/livingstudents' element={<LivingStudents/>}/>
+          <Route path='/hostelexpense' element={<HostelExpense/>}/>
+          <Route path='/studentfee' element={<SudentsFee/>}/>
+          <Route path = '/newemployee' element={<NewEmployee/>}/>
+          <Route path="/updateemployee" element= {<UpdateEmployee/>}/>
+          <Route path="/employeepayment" element={<EmployeePayment/>}/>
+          <Route path="/employeerecord" element={<EmployeeRecord/>} / >
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
